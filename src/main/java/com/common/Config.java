1a84662f66d24475b0522ab1ac62cf06a2c327fd
shinukang <shinu9643@gmail.com>
2026-01-28 00:40:30 +0900

    [Feat] 게시글 작성, 조회 기능 추가

---
+++ src/main/java/com/common/Config.java
@@ -0,0 +1,7 @@
+package com.common;
+
+public class Config {
+    public static final String dbUrl = System.getProperty("db.url");
+    public static final String dbUser = System.getProperty("db.id");
+    public static final String dbPassword = System.getProperty("db.password");
+}
