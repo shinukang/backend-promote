36e9d435396e2fd9d6bfdfd906dfbfacace3be33
shinukang <shinu9643@gmail.com>
2026-01-28 12:04:25 +0900

    [Feat] 댓글 기능 MVC 패턴 적용 - 기존 서블릿(ReplyServlet)을 모델(ReplyDto)과 컨트롤러(ReplyController)로 분리

--- src/main/java/com/api/reply/controller/ReplyController.java
+++ src/main/java/com/api/reply/controller/ReplyController.java
@@ -1,5 +1,6 @@
-package com.api.reply;
+package com.api.reply.controller;
 
+import com.api.reply.model.ReplyDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,8 +14,8 @@ import java.sql.PreparedStatement;
 
 import static com.common.Config.*;
 
-@WebServlet(urlPatterns = {"/reply"})
-public class ReplyServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/reply")
+public class ReplyController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
@@ -22,8 +23,9 @@ public class ReplyServlet extends HttpServlet {
             try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
                 final String sql = "INSERT INTO reply(contents, board_idx) VALUES (?, ?)";
                 try (PreparedStatement pStatement = conn.prepareStatement(sql)) {
-                    pStatement.setString(1, req.getParameter("contents"));
-                    pStatement.setString(2, req.getParameter("board_idx"));
+                    ReplyDto replyDto = ReplyDto.create(req);
+                    pStatement.setString(1, replyDto.getContents());
+                    pStatement.setString(2, replyDto.getBoard_idx());
                     pStatement.executeUpdate();
                     resp.getWriter().write("댓글 작성 완료");
                 }
